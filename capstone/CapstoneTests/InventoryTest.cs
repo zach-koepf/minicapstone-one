51ec3e494f300a4976529a31b0dafc80c437488d
Maria Garcia <mariaamaliagarcia@gmail.com>
2022-10-07 15:01:37 -0400

    More Tests

--- capstone/CapstoneTests/InventoryTest.cs
+++ capstone/CapstoneTests/InventoryTest.cs
@@ -1,17 +1,38 @@
 using Capstone;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Objects;
 
 namespace CapstoneTests
 {
     [TestClass]
     public class InventoryTest
     {
-        [TestMethod]
-        public void Test()
+     [TestMethod]
+
+     public void CheckItemExistsTest()
         {
             Inventory inventory = new Inventory();
+            Item item = new Item("A3", "chips", 5.00M, "chip");
+            bool expectedResult = true;
             
+            bool actualResult = inventory.CheckItemExist(item.SlotId, item);
+            Assert.AreEqual(expectedResult, actualResult);
+
+
 
         }
+        [TestMethod]
+
+    public void CheckItemInStockTest()
+        {
+            Inventory inventory = new Inventory();
+            Item item = new Item("A3", "chips", 5.00M, "chip");
+            bool expectedResult = true;
+            bool actualResult = inventory.CheckItemInStock(item);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+  
     }
 }
